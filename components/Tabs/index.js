87a10c46ed908a03135c76a6c2642d6b633b7174
fuston05 <fuston05@yahoo.com>
2019-12-13 19:33:56 -0500

    finished create tabs part of assignments

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,37 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios
+        .get('https://lambda-times-backend.herokuapp.com/topics')
+        .then(res => {
+                const topics= document.querySelector('.topics');
+                const top= res.data.topics;//destructure
+                // console.log(top);
+
+                top.forEach( ele => {
+                        topics.appendChild(createTab(ele) );
+                        // console.log(ele);
+                })//end foreach
+                // console.log(res);
+        })
+        .catch(err => {
+                console.log('Error: ', err);
+        })
+
+function createTab(topic){
+        //create elements
+        const tab= document.createElement('div');
+
+        //add classes
+        tab.classList.add('tab');
+
+        //add content
+        tab.textContent= topic;
+
+        return tab;
+}//end func
+
+
+
